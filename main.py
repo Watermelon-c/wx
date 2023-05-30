7b4f7c1128a05295a8481522e61e4f6d3b8fe80d
likeiu <1973641241@qq.com>
2023-05-30 14:36:55 +0800

    修改

--- main.py
+++ main.py
@@ -130,6 +130,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
+    love_days = int(love_days) + 1
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
@@ -162,7 +163,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "color": get_color()
             },
             "love_day": {
-                "value": love_days + 1,
+                "value": love_days,
                 "color": get_color()
             },
             "note_en": {
